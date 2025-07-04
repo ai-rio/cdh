f4f41fa613d6b1e6d4039c7a1e23996f397af73a
Carlos Nunes <carlos@ai.rio.br>
2025-07-04 12:24:49 -0300

    refactor(pricing): remove Footer component from pricing page

--- src/app/(frontend)/pricing/page.tsx
+++ src/app/(frontend)/pricing/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
+
 import ParticleCanvas from '../components/ParticleCanvas'
 import { FoundersKeyCard } from '../components/FoundersKeyCard'
 import { PricingCard } from '../components/PricingCard'
@@ -82,7 +82,7 @@ export default function PricingPage() {
       </div>
 
       <PricingPageCtaFooter />
-      <Footer />
+      
 
       <EarlyAccessModal
         isOpen={isEarlyAccessModalOpen}
